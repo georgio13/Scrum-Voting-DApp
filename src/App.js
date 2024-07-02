2980233c0f3610b3c313f41885f468abd25cb577
Yorgos Chatziparaskevas <georgio13@users.noreply.github.com>
2024-07-03 02:17:55 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,121 +1,143 @@
-import React, { Component } from 'react';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import React, {Component} from 'react';
+import voting from './voting';
 import web3 from './web3';
-import lottery from './lottery';
 
-// Η κλάση App αποτελεί απόγονο της Component 
-// η οποία είναι θεμελιώδης στην react.
-// Σε κάθε αλλαγή κάποιας μεταβλητή state της App,
-// όλη η ιστοσελίδα (HTML) γίνεται refresh
-// καλώντας αυτόματα τη render()...
-// ...ΠΡΟΣΟΧΗ! ΔΕΝ γίνεται reload... ΜΟΝΟ refresh
 class App extends Component {
-  state = {
-    manager: '',
-    players: [],
-    balance: '',
-    value: '',
-    message: '',
-    currentAccount: ''
-  };
+    state = {
+        balance: '',
+        currentAccount: '',
+        manager: '',
+        message: '',
+        proposalID: '',
+        proposals: [],
+        remainingVotes: 0,
+        value: ''
+    };
 
-  // Η componentDidMount() καλείται ΜΟΝΟ την πρώτη φορά
-  // που φορτώνει η ιστοσελίδα (είναι σαν την onLoad())
-  async componentDidMount() {
-    try { // Αν υπάρχει εγκατεστημένο metamask
-      // Ορισμός των state μεταβλητών
-      const manager = await lottery.methods.manager().call();
-      const players = await lottery.methods.getPlayers().call();
-      const balance = await web3.eth.getBalance(lottery.options.address);
-      this.setState({ message: '', manager, players, balance });
-      try { // Επικοινωνία με το metamask
-        const currentAccount = (await window.ethereum.request({ method: 'eth_requestAccounts' }))[0];
-        this.setState({ message: '', currentAccount });
-      } catch (error) { // Αν το metamask δεν έκανε accept το request
-        this.setState({ message: 'Metamask has not connected yet' });
-      }
-    } catch (error) { // Αν το metamask δεν έχει εγκατασταθεί
-      console.log(error);
-      this.setState({ message: 'Metamask is not installed' });
+    async componentDidMount() {
+        try {
+            let manager = await voting.methods.manager().call();
+            manager = manager.toLowerCase();
+            const proposals = await voting.methods.getProposals().call();
+            const balance = await web3.eth.getBalance(voting.options.address);
+            this.setState({message: '', manager, proposals, balance});
+            try {
+                const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+                const currentAccount = accounts[0];
+                const voter = await voting.methods.getVoter().call({
+                    from: currentAccount
+                });
+                this.setState({message: '', currentAccount, remainingVotes: Number(voter.remainingVotes)});
+            } catch (error) {
+                this.setState({message: 'Metamask has not connected yet'});
+            }
+        } catch (error) {
+            this.setState({message: 'Metamask is not installed'});
+        }
+        if (!this.eventListenersSet) {
+            this.setupEventListeners();
+            this.eventListenersSet = true;
+        }
     }
-    // Set up event listeners only once
-    if (!this.eventListenersSet) {
-      this.setupEventListeners();
-      this.eventListenersSet = true;
+
+    setupEventListeners() {
+        window.ethereum.on('accountsChanged', async (accounts) => {
+            const currentAccount = accounts[0];
+            const voter = await voting.methods.getVoter().call({
+                from: currentAccount
+            });
+            this.setState({currentAccount, remainingVotes: Number(voter.remainingVotes)});
+        });
+
+        // voting.events.PlayerEntered().on('data', async (data) => {
+        //   console.log(data.returnValues.player);
+        //   const players = await voting.methods.getPlayers().call();
+        //   const balance = await web3.eth.getBalance(voting.options.address);
+        //   this.setState({ players, balance });
+        // });
+
+        // voting.events.WinnerPicked().on('data', async (data) => {
+        //   console.log(data.returnValues.winner);
+        //   const players = await voting.methods.getPlayers().call();
+        //   const balance = await web3.eth.getBalance(voting.options.address);
+        //   const lastWinner = data.returnValues.winner;
+        //   this.setState({ lastWinner, players, balance });
+        // });
     }
-  }
 
-  setupEventListeners() {
-    // Κάθε φορά που επιλέγεται άλλο πορτοφόλι στο metamask...
-    window.ethereum.on('accountsChanged', (accounts) => {
-      // ... να γίνεται refresh η σελίδα
-      const currentAccount = accounts[0];
-      this.setState({ currentAccount });
-    });
+    handleVote = async (proposalID): Promise<any> => {
+        this.setState({message: 'Waiting on transaction success...'});
+        await voting.methods.vote(Number(proposalID)).send({
+            from: this.state.currentAccount,
+            value: web3.utils.toWei('0.01', 'ether')
+        });
+        const voter = await voting.methods.getVoter().call({
+            from: this.state.currentAccount
+        });
+        const balance = await web3.eth.getBalance(voting.options.address);
+        this.setState({message: 'Your vote inserted', balance, remainingVotes: Number(voter.remainingVotes)});
+    };
 
-    // Όποτε αγοραστεί λαχνός να ενημερώσεις τις players & balance
-    lottery.events.PlayerEntered().on('data', async (data) => {
-      console.log(data.returnValues.player);
-      const players = await lottery.methods.getPlayers().call();
-      const balance = await web3.eth.getBalance(lottery.options.address);
-      this.setState({ players, balance });
-    });
+    onClick = async () => {
+        this.setState({message: 'Waiting on transaction success...'});
 
-    // Όποτε γίνει κλήρωση να ενημερώσεις τις players & balance
-    // και να δημιουργήσεις τη νέα state μεταβλητή lastWinner
-    lottery.events.WinnerPicked().on('data', async (data) => {
-      console.log(data.returnValues.winner);
-      const players = await lottery.methods.getPlayers().call();
-      const balance = await web3.eth.getBalance(lottery.options.address);
-      const lastWinner = data.returnValues.winner;
-      this.setState({ lastWinner, players, balance });
-    });
-  }
+        await voting.methods.pickWinner().send({
+            from: this.state.currentAccount
+        });
 
-  // Όταν πατηθεί το κουμπί "Enter"
-  onSubmit = async event => {
-    event.preventDefault();
+        this.setState({message: 'A winner has been picked!'});
+    };
 
-    this.setState({ message: 'Waiting on transaction success...' });
+    isManager = (): boolean => {
+        return this.state.manager === this.state.currentAccount;
+    };
 
-    await lottery.methods.enter().send({ // Κλήση της "enter()" του συμβολαίου
-      from: this.state.currentAccount,
-      value: web3.utils.toWei(this.state.value, 'ether')
-    });
+    render() {
+        return (
+            <div className='contract-container'>
+                <h1 className='title'>Scrum voting DApp</h1>
 
-    this.setState({ message: 'You have been entered!' });
-  };
+                <div className='proposals'>
+                    {this.state.proposals.map(proposal => (
+                        <div className='proposal'>
+                            <h3>{proposal.title}</h3>
+                            <img src={proposal.imageURL} alt={proposal.title}/>
+                            {!this.isManager() && (<button onClick={() => this.handleVote(proposal.id)} type="button"
+                                                           className="btn btn-primary">Vote</button>)}
+                        </div>
+                    ))}
+                </div>
 
-  // Όταν πατηθεί το κουμπί "Pick a Winner"
-  onClick = async () => {
-    this.setState({ message: 'Waiting on transaction success...' });
+                <button type="button" className="btn btn-primary">History</button>
 
-    await lottery.methods.pickWinner().send({ // Κλήση της "pickWinner()" του συμβολαίου
-      from: this.state.currentAccount
-    });
+                <button type="button" className="btn btn-primary">Declare Winner</button>
 
-    this.setState({ message: 'A winner has been picked!' });
-  };
+                <button type="button" className="btn btn-primary">Withdraw</button>
 
-  // Κάθε φορά που η σελίδα γίνεται refresh
-  render() {
-    return (
-      <div>
-        <h2>Lottery Contract</h2>
-        {/* Ό,τι βρίσκεται εντός των άγκιστρων είναι κώδικας JavaScript */}
-        {/* Η σελίδα HLML λειτουργεί αυτόνομα, σαν να εκτελείται σε κάποιον server */}
-        <p>
+                <button type="button" className="btn btn-primary">Reset</button>
+
+                <button type="button" className="btn btn-primary">Change Owner</button>
+
+                <button type="button" className="btn btn-primary">Destroy</button>
+
+                <h4>Connected wallet address: {this.state.currentAccount}</h4>
+
+                <h4>Contract manager: {this.state.manager}</h4>
+
+                <h4>Balance: {parseFloat(web3.utils.fromWei(this.state.balance, 'ether')).toFixed(4)} ether</h4>
+
+                {!this.isManager() && (<h4>Remaining votes: {this.state.remainingVotes}</h4>)}
+
+                {/* <p>
           This contract is managed by {this.state.manager}. There are currently{' '}
-          {this.state.players.length} people entered, competing to win{' '}
+          {this.state.proposals.length} people entered, competing to win{' '}
           {web3.utils.fromWei(this.state.balance, 'ether')} ether!
         </p>
 
-        <hr /> {/*  -------------------- Οριζόντια γραμμή -------------------- */}
+        <hr />
 
-        {/* Η φόρμα "Want to try your luck?" */}
-        {/* Θα μπορούσε αντί να χρησιμοποιηθεί φόρμα, */}
-        {/* να χρησιμοποιηθεί button... */}
-        {/* και αντί .onSubmit να χρησιμοποιούνταν .onClick */}
         <form onSubmit={this.onSubmit}>
           <h4>Want to try your luck? Connected wallet address: {this.state.currentAccount}</h4>
           <div>
@@ -128,20 +150,20 @@ class App extends Component {
           <button>Enter</button>
         </form>
 
-        <hr /> {/*  -------------------- Οριζόντια γραμμή -------------------- */}
+        <hr />
 
         <h4>Ready to pick a winner?</h4>
         <button onClick={this.onClick}>Pick a winner!</button>
 
-        <hr /> {/*  -------------------- Οριζόντια γραμμή -------------------- */}
+        <hr />
 
         <h1>{this.state.message}</h1>
         {this.state.lastWinner &&
           <h3>Last Winner Address: {this.state.lastWinner}</h3>
-        }
-      </div>
-    );
-  }
+        } */}
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/voting.js
@@ -0,0 +1,164 @@
+import web3 from './web3';
+
+const address = '0x9378B6FD286D935f2437Ef1AC4594d78c18987c2';
+
+const abi = [
+	{
+		"inputs": [],
+		"name": "revealWinners",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "proposalID",
+				"type": "uint256"
+			}
+		],
+		"name": "vote",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "winningProposalId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "votingId",
+				"type": "uint256"
+			}
+		],
+		"name": "votingCompleted",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "getProposals",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "uint256",
+						"name": "id",
+						"type": "uint256"
+					},
+					{
+						"internalType": "string",
+						"name": "imageURL",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "title",
+						"type": "string"
+					},
+					{
+						"internalType": "address[]",
+						"name": "voters",
+						"type": "address[]"
+					},
+					{
+						"internalType": "uint256",
+						"name": "votes",
+						"type": "uint256"
+					}
+				],
+				"internalType": "struct Voting.Proposal[]",
+				"name": "",
+				"type": "tuple[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getVoter",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "address",
+						"name": "id",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "remainingVotes",
+						"type": "uint256"
+					}
+				],
+				"internalType": "struct Voting.Voter",
+				"name": "",
+				"type": "tuple"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "manager",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "winners",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "winningProposalId",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+const voting = new web3.eth.Contract(abi, address);
+
+export default voting;
